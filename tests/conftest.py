18411e6321ca8a03aa0e340a03d674bbfee4fad8
Richard Holmes <richard@shedc.uk>
2023-03-22 15:11:23 +0000

    Start with Tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,14 @@
 """Global fixtures for Ambrogio Robot Integration."""
+import pytest
+
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+
+from custom_components.ambrogio_robot.const import DOMAIN
+
+TEST_CONFIGDATA = {DOMAIN: {CONF_USERNAME: "user.name", CONF_PASSWORD: "hash"}}
+
+
+@pytest.fixture
+def mock_configdata():
+    """Return a default mock configuration."""
+    return TEST_CONFIGDATA
--- tests/test_init.py
+++ tests/test_init.py
@@ -2,6 +2,12 @@
 
 from homeassistant.core import HomeAssistant
 
+from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-async def test_setup(hass: HomeAssistant):
+from custom_components.ambrogio_robot.const import DOMAIN
+
+
+async def test_setup(hass: HomeAssistant, mock_configdata: dict):
     """Test we can setup the integration."""
+    entry = MockConfigEntry(domain=DOMAIN, data=mock_configdata[DOMAIN])
+    entry.add_to_hass(hass)
